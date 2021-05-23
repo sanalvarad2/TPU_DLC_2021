aecd3c8679dd44287efc689b66facbd7af13b405
Santiago Alvarado <santiagoalvarado96@gmail.com>
2021-05-23 20:36:48 -0300

    Primer Commit

---
+++ src/main/java/ENT/Documento.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ENT;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Santiago
+ */
+@Entity
+@Table(name = "documento")
+@NamedQueries({
+    @NamedQuery(name = "Documento.findAll", query = "SELECT d FROM Documento d"),
+    @NamedQuery(name = "Documento.findById", query = "SELECT d FROM Documento d WHERE d.id = :id")})
+public class Documento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Long id;
+
+    public Documento() {
+    }
+
+    public Documento(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Documento)) {
+            return false;
+        }
+        Documento other = (Documento) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ENT.Documento[ id=" + id + " ]";
+    }
+    
+}
